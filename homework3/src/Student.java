d499144c06b30dfa860ec550b9c8b8e58675f55d
lisssachka <114672738+lisssachka@users.noreply.github.com>
2023-12-12 16:44:49 +0400

    Added HW3

---
+++ homework3/src/Student.java
@@ -0,0 +1,26 @@
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+    private final String name;
+    private final int age;
+    private final transient double GPA;
+
+    public Student(String name, int age, double GPA) {
+        this.name = name;
+        this.age = age;
+        this.GPA = GPA;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+}
