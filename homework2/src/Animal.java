615e45d890de3bcd83275de49289f25ea9fa1353
lisssachka <114672738+lisssachka@users.noreply.github.com>
2023-12-12 16:44:29 +0400

    Added HW2

---
+++ homework2/src/Animal.java
@@ -0,0 +1,21 @@
+/*
+Абстрактный класс Animal
+ */
+public abstract class Animal {
+    public String name;
+    public int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
